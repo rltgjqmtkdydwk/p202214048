2be04244320f78b12b9033b8d30b623a97e3377a
rltgjqmtkdydwk <slvrcods22@gmail.com>
2024-11-20 09:48:28 +0900

    three

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
       System.out.println("202214048 김은총");
       System.out.println(Hello.sum(10));
       System.out.println(Hello.sum(100));
+      System.out.println(Hello.sum(1000));
     }
   }  
